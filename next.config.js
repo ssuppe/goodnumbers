029bf49118c34c10271b0749c2ebcda49c23fa9a
Steve Suppe <you@example.com>
2024-10-20 02:25:43 +0100

    l

--- next.config.js
+++ next.config.js
@@ -2,18 +2,6 @@ module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   poweredByHeader: false,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'source.unsplash.com',
-      },
-    ],
-  },
   compiler: { styledComponents: true },
   rewrites: async () => {
     return [
