30368e5d1b48de1d58eee9aa578f3191bf87ba13
Steve Suppe <ssuppe@gmail.com>
2024-11-03 20:47:06 +0000

    Fixed cli get_op_status.py

--- bgpodcast/audio/get_op_status.py
+++ bgpodcast/audio/get_op_status.py
@@ -1,25 +1,37 @@
-# import time
+import time
+import sys
+import pprint
+import asyncio
+import gcloud
 # from fastapi import HTTPException
 # from google.api_core import client_options
 # import google.cloud.texttospeech as tts
 # from pydantic import BaseModel
 
 
-# async def test(result):
-#      print(result)
-#      # Check status periodically
-#      start_time = time.time()
-#      while time.time() - start_time < TIMEOUT:
-#           status = await get_job_status(result)
-#           print("Current status:", status)
-          
-#           if status["status"] in ["complete", "error"]:
-#                break
-               
-#           await asyncio.sleep(POLLING_INTERVAL)
+
+async def test(op_id : str):
+    # Check status periodically
+    start_time = time.time()
+    while time.time() - start_time < 300:
+        status = await gcloud.get_job_status(op_id)
+        print(f"\nCurrent status (after {
+              int(time.time() - start_time)} seconds):")
+        pprint.pprint(status)
+
+        if status.status == "done":
+            print("All done!")
+
+            # print(status["result"])
+        elif status.status == "error":
+            print("Error!")
+            print(status["error"])
+        else:
+            print("Still processing...")
+            print(status)
+
+        await asyncio.sleep(10)
 
 
-# import asyncio
-# import sys
-# import ast
-# q
+if __name__ == "__main__":
+    asyncio.run(test(sys.argv[1]))
