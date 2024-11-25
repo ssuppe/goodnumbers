888f3cbf7f8ed14a9de8b1f960b32dbfac8cd51f
Steve Suppe <ssuppe@gmail.com>
2024-11-25 22:54:58 +0000

    Finally got state working

--- src/types/nightscout.ts
+++ src/types/nightscout.ts
@@ -1,19 +1,19 @@
 export interface PodcastGenerateResult {
-    status: string;
-    operation_id?: string;
-    url?: string;
-    error?: string;
-    gcs_path?: string;
-    bucket_name?: string;
-  }
+  status: string;
+  operation_id?: string;
+  url?: string;
+  error?: string;
+  gcs_path?: string;
+  bucket_name?: string;
+}
 
 export interface AssessmentData {
-  notes: string;
-  assessment1: string;
-  assessment2: string;
-  dialog: string;
-  podcastResult: PodcastGenerateResult;
-  timestamp : string | null;
+  notes: string | null;
+  assessment1: string | null;
+  assessment2: string | null;
+  dialog: string | null;
+  podcastResult: PodcastGenerateResult | null;
+  timestamp: string | null;
 }
 
 export interface InitialStoredData {
@@ -22,5 +22,5 @@ export interface InitialStoredData {
   assessment2: string | null;
   dialog: string | null;
   podcastResult: PodcastGenerateResult | null;
-  timestamp : string | null;
+  timestamp: string | null;
 }
