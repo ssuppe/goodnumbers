0c8f8017328f1328988a8ee1cead0811aee45a54
Steve Suppe <ssuppe@gmail.com>
2024-11-27 19:19:11 +0000

    footer comments

--- src/components/widgets/Footer.tsx
+++ src/components/widgets/Footer.tsx
@@ -20,7 +20,7 @@ const Footer = () => {
                 {links &&
                   links.map(({ label, href }, index) => (
                     <li key={`item-link-${index}`}>
-                      <Link
+                      <a
                         className="duration-150 ease-in-out placeholder:transition hover:text-gray-700 hover:underline dark:text-gray-400"
                         aria-label={label}
                         href={href}
@@ -31,10 +31,10 @@ const Footer = () => {
                     </li>
                   ))}
               </ul>
-            </div>
-          </div> */}
-          {/* {columns.map(({ title, links }, index) => ( */}
-            {/* <div key={`item-column-${index}`} className="col-span-6 md:col-span-3 lg:col-span-2">
+            </div> */}
+          </div>
+          {/* {columns.map(({ title, links }, index) => (
+            <div key={`item-column-${index}`} className="col-span-6 md:col-span-3 lg:col-span-2">
               <div className="mb-2 font-medium dark:text-gray-300">{title}</div>
               <ul className="text-sm">
                 {links &&
@@ -51,22 +51,22 @@ const Footer = () => {
                   ))}
               </ul>
             </div> */}
-          {/* ))} */}
+          ))}
         </div>
         <div className="py-6 md:flex md:items-center md:justify-between md:py-8">
-          <ul className="mb-4 flex md:order-1 md:ml-4 rtl:md:ml-0 rtl:md:mr-4 md:mb-0">
-            {/* {socials.map(({ label, icon: Icon, href }, index) => (
-              // <li key={`item-social-${index}`}>
-              //   <a
-              //     className="text-muted inline-flex items-center rounded-lg p-2.5 text-sm hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-              //     aria-label={label}
-              //     href={href}
-              //   >
-              //     {Icon && <Icon className="h-5 w-5" />}
-              //   </a>
-              // </li>
-            ))} */}
-          </ul>
+          {/* <ul className="mb-4 flex md:order-1 md:ml-4 rtl:md:ml-0 rtl:md:mr-4 md:mb-0">
+            {socials.map(({ label, icon: Icon, href }, index) => (
+              <li key={`item-social-${index}`}>
+                <a
+                  className="text-muted inline-flex items-center rounded-lg p-2.5 text-sm hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+                  aria-label={label}
+                  href={href}
+                >
+                  {Icon && <Icon className="h-5 w-5" />}
+                </a>
+              </li>
+            ))}
+          </ul> */}
           {footNote}
         </div>
       </div>
