be93588173046e0c1785ded80ac72a93d2466361
Steve Suppe <ssuppe@gmail.com>
2024-11-27 19:07:09 +0000

    commented out unnecessary footers, causing vercel issues

--- src/components/widgets/Footer.tsx
+++ src/components/widgets/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { footerData } from '~/shared/data/global.data';
 
 const Footer = () => {
@@ -10,16 +11,16 @@ const Footer = () => {
         <div className="grid grid-cols-12 gap-4 gap-y-8 py-8 sm:gap-8 md:py-12">
           <div className="col-span-12 lg:col-span-4">
             <div className="mb-2">
-              <a className="inline-block text-xl font-bold" href="/">
+              <Link className="inline-block text-xl font-bold" href="/">
                 {title}
-              </a>
+              </Link>
             </div>
-            <div className="text-muted text-sm">
+            {/* <div className="text-muted text-sm">
               <ul className="mb-4 flex pr-2 rtl:pr-0 rtl:pl-2 md:order-1 md:mb-0">
                 {links &&
                   links.map(({ label, href }, index) => (
                     <li key={`item-link-${index}`}>
-                      <a
+                      <Link
                         className="duration-150 ease-in-out placeholder:transition hover:text-gray-700 hover:underline dark:text-gray-400"
                         aria-label={label}
                         href={href}
@@ -31,39 +32,39 @@ const Footer = () => {
                   ))}
               </ul>
             </div>
-          </div>
+          </div> */}
           {columns.map(({ title, links }, index) => (
-            <div key={`item-column-${index}`} className="col-span-6 md:col-span-3 lg:col-span-2">
-              <div className="mb-2 font-medium dark:text-gray-300">{title}</div>
-              <ul className="text-sm">
-                {links &&
-                  links.map(({ label, href }, index2) => (
-                    <li key={`item-column-link-${index2}`} className="mb-2">
-                      <a
-                        className="text-muted transition duration-150 ease-in-out hover:text-gray-700 hover:underline dark:text-gray-400"
-                        aria-label={label}
-                        href={href}
-                      >
-                        {label}
-                      </a>
-                    </li>
-                  ))}
-              </ul>
-            </div>
+            // <div key={`item-column-${index}`} className="col-span-6 md:col-span-3 lg:col-span-2">
+            //   <div className="mb-2 font-medium dark:text-gray-300">{title}</div>
+            //   <ul className="text-sm">
+            //     {links &&
+            //       links.map(({ label, href }, index2) => (
+            //         <li key={`item-column-link-${index2}`} className="mb-2">
+            //           <a
+            //             className="text-muted transition duration-150 ease-in-out hover:text-gray-700 hover:underline dark:text-gray-400"
+            //             aria-label={label}
+            //             href={href}
+            //           >
+            //             {label}
+            //           </a>
+            //         </li>
+            //       ))}
+            //   </ul>
+            // </div>
           ))}
         </div>
         <div className="py-6 md:flex md:items-center md:justify-between md:py-8">
           <ul className="mb-4 flex md:order-1 md:ml-4 rtl:md:ml-0 rtl:md:mr-4 md:mb-0">
             {socials.map(({ label, icon: Icon, href }, index) => (
-              <li key={`item-social-${index}`}>
-                <a
-                  className="text-muted inline-flex items-center rounded-lg p-2.5 text-sm hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
-                  aria-label={label}
-                  href={href}
-                >
-                  {Icon && <Icon className="h-5 w-5" />}
-                </a>
-              </li>
+              // <li key={`item-social-${index}`}>
+              //   <a
+              //     className="text-muted inline-flex items-center rounded-lg p-2.5 text-sm hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-700"
+              //     aria-label={label}
+              //     href={href}
+              //   >
+              //     {Icon && <Icon className="h-5 w-5" />}
+              //   </a>
+              // </li>
             ))}
           </ul>
           {footNote}
