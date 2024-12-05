c9241d122056c51557a2c7ed5b1bd3da3841a3db
Steve Suppe <ssuppe@gmail.com>
2024-12-05 23:59:42 +0000

    Finetuning

--- bgpodcast/data_analysis/dfutils.py
+++ bgpodcast/data_analysis/dfutils.py
@@ -1,5 +1,24 @@
+from datetime import timedelta
 import ruptures as rpt
 import pandas as pd
+
+def prepare_data(data, days_to_analyze=7) -> pd.DataFrame:
+    df = pd.DataFrame(data)
+    
+    # Apply UTC offset
+    df['adjusted_date'] = df.apply(lambda row: row['date'] + (row['utcOffset'] * 60 * 1000), axis=1)
+    df['datetime'] = pd.to_datetime(df['adjusted_date'], unit='ms')
+    
+    # Filter to last 7 days
+    latest_date = df['datetime'].max()
+    start_date = latest_date - timedelta(days=days_to_analyze)
+    df = df[df['datetime'] >= start_date]
+    
+    # Sort and add date
+    df = df.sort_values('datetime')
+    df['date'] = df['datetime'].dt.date
+    
+    return df
 
 def calculate_average_day(insulin_sgv, period='30min', quantile=0.8):
     """
--- bgpodcast/data_analysis/weekly_analysis.py
+++ bgpodcast/data_analysis/weekly_analysis.py
@@ -1,33 +1,15 @@
-from datetime import timedelta
-
-import pandas as pd
-
+from dfutils import prepare_data
 from bgpodcast.utils import bgutils
 
 
-def prepare_data(entries, days_to_analyze=7):
-    df = pd.DataFrame(entries)
-    
-    # Apply UTC offset
-    df['adjusted_date'] = df.apply(lambda row: row['date'] + (row['utcOffset'] * 60 * 60 * 1000), axis=1)
-    df['datetime'] = pd.to_datetime(df['adjusted_date'], unit='ms')
-    
-    # Filter to last 7 days
-    latest_date = df['datetime'].max()
-    start_date = latest_date - timedelta(days=days_to_analyze)
-    df = df[df['datetime'] >= start_date]
-    
-    # Sort and add date
-    df = df.sort_values('datetime')
-    df['date'] = df['datetime'].dt.date
-    
-    return df
+
 
 def weekly_stats(entries) -> str:
     notes = ""
     entries = prepare_data(entries, days_to_analyze=7)
-    mean, stddev, pct_low, pct_high, tir, ttir = bgutils.get_sgv_stats(entries)
+    start_date, end_date, mean, stddev, pct_low, pct_high, tir, ttir = bgutils.get_sgv_stats(entries)
     
+    notes += f"Report from {start_date} to {end_date}"
     notes += f"The average blood glucose for was {mean:.0f} mg/dl, with a standard deviation of {stddev:.1f} mg/dl\n"
     notes += f"This means that 95% of the time, blood glucose was between {(mean - stddev*2):.0f} and {(mean + stddev*2):.0f}.\n"
     notes += f"Time in range was {tir:.0%} mg/dl.\n"
