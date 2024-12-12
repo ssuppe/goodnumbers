ed02c529b0686bd4c473c1dedcac8fae23404021
Steve Suppe <ssuppe@gmail.com>
2024-12-12 00:09:20 +0000

    removed api rewrites i no longer use now that i host on gcloud

--- goodnumbers/next.config.js
+++ goodnumbers/next.config.js
@@ -3,15 +3,5 @@ module.exports = {
   swcMinify: true,
   poweredByHeader: false,
   compiler: { styledComponents: true },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination:
-          process.env.NODE_ENV === "development"
-            ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/:path*", // Important: Point to the Vercel function route in production
-      },
-    ];
-  }
+  productionBrowserSourceMaps: true,
 };
