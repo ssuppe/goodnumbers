8a4c6d836620129a33658d999200813bce67d07a
Steve Suppe <ssuppe@gmail.com>
2024-12-30 18:07:46 +0000

    Added color to the progress bar

--- goodnumbers/src/components/ui/progress/index.tsx
+++ goodnumbers/src/components/ui/progress/index.tsx
@@ -1,19 +1,16 @@
-import React from "react";
-import * as RadixProgress from "@radix-ui/react-progress";
-import "./styles.css";
+import React from 'react';
+import * as RadixProgress from '@radix-ui/react-progress';
+import './styles.css';
 
 interface ProgressProps {
   value: number;
   className?: string;
 }
 
-const Progress: React.FC<ProgressProps> = ({ value, className = "" }) => {
+const Progress: React.FC<ProgressProps> = ({ value, className = '' }) => {
   return (
     <RadixProgress.Root className={`ProgressRoot ${className}`} value={value}>
-      <RadixProgress.Indicator
-        className="ProgressIndicator"
-        style={{ transform: `translateX(-${100 - value}%)` }}
-      />
+      <RadixProgress.Indicator className="ProgressIndicator" style={{ transform: `translateX(-${100 - value}%)` }} />
     </RadixProgress.Root>
   );
 };
