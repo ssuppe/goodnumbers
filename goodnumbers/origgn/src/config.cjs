25c021bf0c1a33d3ec8dfe01ff005ef0d66d023b
Steve Suppe <ssuppe@gmail.com>
2025-04-04 13:55:20 +0100

    moved origgn out of repo into home dir

--- goodnumbers/origgn/src/config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports.SITE = {
-  name: 'GoodNumbers',
-
-  title: 'GoodNumbers — Your personalized week in blood sugar management',
-  description:
-    'GoodNumbers is a personalized podcast about your blood sugar levels. Analyzed with statistical algorithms, we create an AI-generated podcast you can listen to, identifying your highs, lows, and strategies to address them.',
-};
