97ee9e1a954d318686e87cef437b4f57487e7b49
Steve Suppe <ssuppe@gmail.com>
2025-04-11 14:26:31 +0100

    Changes

--- goodnumbers/next.config.js
+++ goodnumbers/next.config.js
@@ -2,6 +2,12 @@
 const nextConfig = {
   /* config options here */
   // reactStrictMode: true, // Example: Add options here if needed
+  webpack: (config, { dev, isServer }) => {
+    if (dev) {
+      config.devtool = 'eval-source-map';
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
