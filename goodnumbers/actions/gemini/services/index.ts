44879934cbf044d2b8056f83fde670673286fdb0
Steve Suppe <ssuppe@gmail.com>
2025-04-14 18:47:27 +0100

    refactor: reorganize geminiActions.ts
    I'd like to refactor geminiActions.ts. It was originally a file for my calls to Google Gemini API, but it has utility classes and other things that I think should be reorganized. Can you please read the file and make suggestions?
    
    ```git-revs
    ee1c9d9  (Base revision)
    644d861  Create assessmentService.ts
    9b7c222  Create podcastService.ts
    77034a7  Update services/index.ts to export service functions
    02b1ec6  Update index.ts to export from services
    HEAD     Deprecate geminiActions.ts with a compatibility shim
    ```
    
    codemcp-id: 37-refactor-reorganize-geminiactions-ts

--- goodnumbers/actions/gemini/services/index.ts
+++ goodnumbers/actions/gemini/services/index.ts
@@ -1,1 +1,9 @@
 // Export services from this directory
+export { getAssessment } from './assessmentService';
+export { 
+  generatePodcastText,
+  generatePodcastDescription,
+  generatePodcastAudio,
+  checkPodcastStatus,
+  type Description
+} from './podcastService';
