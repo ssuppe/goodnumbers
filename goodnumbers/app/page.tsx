0015cf62372a37472bacef7d156b891be45247f4
Steve Suppe <ssuppe@gmail.com>
2025-04-19 21:55:12 +0100

    New top page content, small layout changes

--- goodnumbers/app/page.tsx
+++ goodnumbers/app/page.tsx
@@ -1,24 +1,25 @@
 import Hero from '@/components/widgets/Hero';
 
 const heroHome = {
-  title: <>Personalized podcast about your weekly diabetes numbers</>,
+  title: <>A smart weekly journal for type 1 diabetics</>,
   subtitle: (
     <>
       <span className="hidden md:inline">
-        <span className="font-semibold underline decoration-primary-600 decoration-wavy decoration-1 underline-offset-2">
-          GoodNumbers
-        </span>{' '}
-        is an experimental weekly personalized podcast about your blood sugar levels. We use non-AI statistical
-        algorithms to analyze your blood sugar levels and creates an AI-generated podcast for you to listen to,
-        discussing your highs, lows, and strategies to address them. Use it for self-reflection, to find your blind
-        spots to your diabetes management, and to continuously improve.
+        <span className="font-semibold decoration-primary-600 decoration-1">GoodNumbers</span>
+        &nbsp;is an <span className="underline">experimental</span> weekly journal to help type 1 diabetics reflect and
+        improve their blood sugar levels week to week. It uses a mix of good old statistical analysis to help you zero
+        in on troublesome trends and identify patterns. It then leverages AI to help you reflect on strategies to
+        address them.
       </span>
+      <div className="pt-4">
+        Use it for self-reflection, to find your blind spots in your diabetes management, and to continuously improve.
+      </div>
     </>
   ),
   callToAction: {
-    text: 'Try Demo',
+    text: 'Try Nightscout Demo',
     href: 'trynow',
-    icon: undefined, //IconBrandApplePodcast,
+    // icon: IconBrandApplePodcast,
     targetBlank: false,
   },
   callToAction2: {
--- goodnumbers/components/widgets/Hero.tsx
+++ goodnumbers/components/widgets/Hero.tsx
@@ -7,7 +7,7 @@ const Hero = ({ title, subtitle, tagline, callToAction, callToAction2, image }: 
   return (
     <section id="heroOne">
       <div className="mx-auto max-w-7xl px-4 sm:px-6">
-        <div className="py-12 md:py-20">
+        <div className="py-4 md:py-4">
           <div className="mx-auto max-w-4xl pb-10 text-center md:pb-16">
             {tagline && (
               <p className="text-base font-semibold uppercase tracking-wide text-primary-600 dark:text-primary-200">
