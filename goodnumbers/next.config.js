cde597b2f90b2bd755637018bd4c66e7355a384b
Steve Suppe <ssuppe@gmail.com>
2025-07-16 00:29:03 +0100

    Commented out log debug statements in Chrome console debugger

--- goodnumbers/next.config.js
+++ goodnumbers/next.config.js
@@ -9,6 +9,27 @@ const nextConfig = {
     return config;
   },
   output: 'standalone',
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+              default-src 'self';
+              media-src 'self' https://storage.googleapis.com;
+              connect-src 'self' https://*.clerk.accounts.dev https://*.clerk.dev https://api.clerk.dev https://api.iconify.design https://api.simplesvg.com https://api.unisvg.com;
+              img-src 'self' data: blob:;
+              script-src 'self' 'unsafe-eval' 'unsafe-inline';
+              style-src 'self' 'unsafe-inline';
+              font-src 'self';
+            `.replace(/\s{2,}/g, ' ').trim(),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
