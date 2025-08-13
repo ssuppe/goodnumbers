f1389b89d18e03edbb97a42719e49d4f62653607
Steve Suppe <ssuppe@gmail.com>
2025-08-13 01:31:54 +0100

    chore: Archive existing proof-of-concept code

--- goodnumbers/config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports.SITE = {
-  name: 'GoodNumbers',
-
-  title: 'GoodNumbers — A smart weekly journal for type 1 diabetics',
-  description:
-    'GoodNumbers is a personalized weekly report about your blood sugar levels. Analyzed with statistical algorithms, we create an AI-generated podcast you can listen to, identifying your highs, lows, and strategies to address them.',
-};
